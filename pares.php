131ceb1abbb300b1ee24424ef0cada0e2b1cdb58
aluno <aluno@lab-wbs.local>
2016-03-30 08:06:02 -0300

    foco força e fe

---
+++ pares.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<title>Maria</title>
+	</head>
+	<body>
+	<p>Numeros pares</p>
+	</body>
+<?php
+$num1;
+$num2 = 2;
+
+	
+	
+	
+for($num1 =1; $num1 < 20; $num1++){
+ if(($num1 % $num2) == 0) {
+ echo"<br/>Numero Par: ".$num1;
+ 
+ 
+ }
+
+
+}
+	
+?>
+</html>
