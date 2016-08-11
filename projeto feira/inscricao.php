22c661c67842712ceff80130caa1a88a41fe3812
aluno <aluno@labwbs09>
2016-08-11 08:49:03 -0300

    11/08

--- projeto feira/inscricao.php
+++ projeto feira/inscricao.php
@@ -1,1 +1,19 @@
 <?php
+	echo "GET:" . var_dump($_GET) . "<br>";
+	echo "POST:" . var_dump($_POST) . "<br>";
+	echo "posicao nome" . $_POST['nome'];
+	
+	
+	/*configuracao de enderecamento do banco de dados*/
+	$servidor = "localhost";
+	$usuario = "root";
+	$senha = "";
+	
+	/*configuracao de acesso do banco de dados*/
+	$nome_banco = "bd_centro_interesse";
+	
+	$conexao = mysql_connect($servidor, $usuario, $senha);
+	
+	$banco = mysql_select_db($nome_banco, $conexao);
+	
+?>
