f530d609777efe4d09ca9de9ffde4c9cc70100fb
aluno <aluno@labwbs17>
2016-08-18 07:42:19 -0400

    alteraçao do codigo 18/08

--- projeto feira/inscricao.php
+++ projeto feira/inscricao.php
@@ -1,27 +1,35 @@
 <?php
-	echo "GET:" . var_dump($_GET) . "<br>";
-	echo "POST:" . var_dump($_POST) . "<br>";
-	echo "posicao nome" . $_POST['nome'];
+	echo var_dump($_GET) . "GET <br>";
+	echo var_dump($_POST) . "POST <br>";
+	$nome = $_POST['nome'];
+	$codigo=  $_POST['meucodigo'];
+	$opcao1 = $_POST['opcao1'];
+	$opcao2 = $_POST['opcao2'];
 	
 	
-	/*configuracao de enderecamento do banco de dados*/
+	/*configurco de enderecmento do bnco de ddos*/
 	$servidor = "localhost";
 	$usuario = "root";
 	$senha = "";
 	
-	/*configuracao de acesso do banco de dados*/
+	/*configurco de cesso o bnco de ddos*/
 	$nome_banco = "bd_centro_interesse";
 	
 	$conexao = mysql_connect($servidor, $usuario, $senha);
 	
-	$banco = mysql_select_db($nome_banco, $conexao);
-	
 	/*verifica se a conexao realmente foi criada*/
-	/*se (conexao) entao, ou seja, conexao e falsa*/
+	/*se (nao conexao) entao, ou seja, conexao e falsa*/
 	if (!$conexao) {
-		echo "nao foi possivel conectar ao banco MYSQL.";
+		echo "Não foi possível connectar ao servidor";
 		exit;
 	}else{/*senao*/
-		echo " conectou!";
+		echo "<h1>Conectou!</h1>";
 	}
+	
+	/*Selecione o banco de dados ou morra*/
+	$banco = mysql_select_db($nome_banco, $conexao) or die ("Não foi possível conectar ao banco de dados");
+	
+	
+	/*Encerra a conexao*/
+	mysql_close();
 ?>
