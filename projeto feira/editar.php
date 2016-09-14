09c2c635aaa2d016f2d449b4ed365172b514014d
aluno <aluno@labwbs09>
2016-09-14 09:39:31 -0300

    Ediçao do codigo 14/09

--- projeto feira/editar.php
+++ projeto feira/editar.php
@@ -21,36 +21,51 @@ $quantidade_vagas = $resultado['quantidade_vagas'];
 
 
 ?>
-<form id="form1" name="form1" method="post" action="atualizar_centro.php">
-<table width="400" border="0" align="center">
-<tr><td width="1">nome:</td>
-<td width="400"><input name="nome" type="text" size="50" id="nome" maxlength="100" value="<?php echo $nome; ?>" /></td>
-</tr>
+   <form action="index.html" method="post" >
+				<div class="row form-group">
+					<div class="col-md-6">
+						<label for="nome-do-aluno">Nome:</label>
+						<input type="text" id="nome-do-aluno"  class="form-control"/> 
+					</div>
+				</div>	
 
-<tr><td width="1">descricao:</td>
-<td width="400"><input name="descricao" type="text" size="50" id="descricao" maxlength="100" value="<?php echo $descricao; ?>" /></td>
-</tr>
+   <div class="row form-group">
+					<div class="col-md-6">
+						<label for="descricao">Descricao:</label>
+						<input type="text" id="descricao"  class="form-control"/> 
+					</div>
+				</div>
+				
+   <div class="row">
+					<div class="form-group">
+						<div class="col-md-2">
+							<label for="horario-de-inicio">Horario de Inicio:</label>
+							<input type="time" id="horario-de-inicio"  class="form-control"/> 
+						</div>
+						<div class="col-md-2">
+							<label for="horario-de-termino">Horario de Termino:</label>
+							<input type="time" id="horario-de-termino"  class="form-control"/> 
+						</div>
+					</div>
+				</div>
 
-<tr><td width="1">horario inicio:</td>
-<td width="400"><input name="horario_inicio" type="text" size="50" id="horario_inicio" maxlength="100" value="<?php echo $horario_inicio; ?>" /></td>
-</tr>
+   <div class="row form-group">
+					<div class="col-md-6">
+						<label for="orientador">Orientador:</label>
+						<input type="text" id="orientador"  class="form-control"/> 
+					</div>
+				</div> 
 
-<tr><td width="1">horario termino:</td>
-<td width="400"><input name="horario_termino" type="text" size="50" id="horario_termino" maxlength="100" value="<?php echo $horario_termino; ?>" /></td>
-</tr>
+   <div class="row form-group">
+					<div class="col-md-2">
+						<label for="vagas">Vagas:</label>
+						<input type="text" id="vagas"  class="form-control"/> 
+					</div>
+				</div>
 
-<tr><td width="1">orientador:</td>
-<td width="400"><input name="orientador" type="text" size="50" id="orientador" maxlength="100" value="<?php echo $orientador; ?>" /></td>
-</tr>
-
-<tr><td width="1">quantidade vagas:</td>
-<td width="400"><input name="quantidade_vagas" type="text" size="50" id="quantidade_vagas" maxlength="100" value="<?php echo $quantidade_vagas; ?>" /></td>
-</tr>
-
-<input type="submit" name="submit" value="Salvar"></td>
-</tr>
-</table>
-</form>
+<button type="button" class="btn btn-success">
+	  <span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Salvar
+		</button>
 
 
 
